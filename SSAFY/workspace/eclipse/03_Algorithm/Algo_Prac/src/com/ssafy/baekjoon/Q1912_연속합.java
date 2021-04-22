664579086db3e258b1b7125ce211c031246dcaf0
정범진 <last9500@naver.com>
2021-04-23 00:17:48 +0900

    algorithm

--- SSAFY/workspace/eclipse/03_Algorithm/Algo_Prac/src/com/ssafy/baekjoon/Q1912_연속합.java
+++ SSAFY/workspace/eclipse/03_Algorithm/Algo_Prac/src/com/ssafy/baekjoon/Q1912_연속합.java
@@ -7,35 +7,25 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q1912_연속합 {
-	static int C, N, sum = 0;
-	static int[] numbers, result;
-	static boolean[] check;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		numbers = new int[N];
-		result = new int[2];
-		check = new boolean[N];		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
-			numbers[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		permutation(0, 0);
-	}
-	static void permutation(int cnt, int start) {
-		if(cnt == C) {
-			System.out.println(Arrays.toString(result));
-			return;
+		int[] dp = new int[N];
+		dp[0] = arr[0];
+		for(int i = 1; i < N; i++) {
+			if(dp[i - 1] + arr[i] > arr[i]) {
+				dp[i] = dp[i - 1] + arr[i];
+			} else {
+				dp[i] = arr[i];
+			}
 		}
-		for(int i = start; i < N; i++) {
-			if(check[i]) continue;
-			
-			result[cnt] = numbers[i];
-			check[i] = true;
-			
-			permutation(cnt+1, i+1);
-			check[i] = false;
-		}
-		
+		Arrays.sort(dp);
+		System.out.println(dp[N - 1]);
 	}
 }
